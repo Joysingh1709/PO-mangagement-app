da91539919cf2cabce162c63976e87f51bfb4b3c
Joysingh1709 <avnishmamta060606@gmail.com>
2025-11-14 04:15:18 +0530

    fix: update greeting message to include version for Orders and Products Management Services

--- services/orders-management-service/src/app.service.ts
+++ services/orders-management-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from Orders Management Service!';
+    return 'Hello from Orders Management Service V1!';
   }
 }
--- services/products-management-service/src/app.service.ts
+++ services/products-management-service/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from Products Management Service!';
+    return 'Hello from Products Management Service V1!';
   }
 }
